0ed4a1af5dc5d68c72538e517284eca0185ad099
bojidarStanoev <bojidar.stan@gmail.com>
2018-03-25 15:27:17 +0300

    first steps

---
+++ WebApplication1/WebApplication1/Controllers/QuestionController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class QuestionController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Question/
+        public ActionResult Index()
+        {
+            return View(db.Questions.ToList());
+        }
+
+        // GET: /Question/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // GET: /Question/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Question/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,User_Id,title,vote,body")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Questions.Add(question);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(question);
+        }
+
+        // GET: /Question/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // POST: /Question/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,User_Id,title,vote,body")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(question).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(question);
+        }
+
+        // GET: /Question/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // POST: /Question/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Question question = db.Questions.Find(id);
+            db.Questions.Remove(question);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int User_Id { get; set; }
+
+        public string title { get; set; }
+       
+        public int vote { get; set; }
+
+        public string body { get; set; }
+    }
+}
